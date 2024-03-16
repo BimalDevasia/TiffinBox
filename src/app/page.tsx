41c70b42eaf8bffe308b3582a1a91e709bed721f
BimalDevasia <bimaldevasia@gmail.com>
2024-03-16 18:33:04 +0530

    jh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
+    <>
   <div>hello</div>
-
+  <Link href="/specific-route">
+  <a>Go to Specific Route</a>
+</Link>
+    </>
   );
 }
